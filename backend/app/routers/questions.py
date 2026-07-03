9f6ca91396d318f7a548167a943a3f55a6174eed
rudrakshia30 <rudrakshia30@gmail.com>
2026-07-03 21:23:08 +0530

    questions router

--- backend/app/routers/questions.py
+++ backend/app/routers/questions.py
@@ -1,3 +1,5 @@
 from fastapi import APIRouter, HTTPException
 from app.schemas import (QuestionGenerationRequest, QuestionGenerationResponse)
 from app.services.groq_services import generate_interview_questions
+
+router = APIRouter(prefix="/api/questions", tags=["Interview Questions"])
