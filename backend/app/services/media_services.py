2c3ecdec85cacf43cf4d2d84e206f1e5b2f65a78
rudrakshia30 <rudrakshia30@gmail.com>
2026-07-07 22:31:55 +0530

    allowed extensions

--- backend/app/services/media_services.py
+++ backend/app/services/media_services.py
@@ -6,3 +6,4 @@ from fastapi import UploadFile
 
 CHUNK_SIZE = 1024 * 1024
 MAX_VIDEO_SIZE_BYTES = int(os.getenv("MAX_VIDEO_UPLOAD_MB","200")) * 1024 * 1024
+ALLOWED_VIDEO_EXTENSIONS = {".webm",".mp4",".mov",".m4v",".mkv",".mpeg",".mpg"}
