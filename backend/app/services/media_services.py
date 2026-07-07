d5e795b7f9952ea291d186940d972a66cee7e0a7
rudrakshia30 <rudrakshia30@gmail.com>
2026-07-07 22:33:01 +0530

    error function

--- backend/app/services/media_services.py
+++ backend/app/services/media_services.py
@@ -7,3 +7,7 @@ from fastapi import UploadFile
 CHUNK_SIZE = 1024 * 1024
 MAX_VIDEO_SIZE_BYTES = int(os.getenv("MAX_VIDEO_UPLOAD_MB","200")) * 1024 * 1024
 ALLOWED_VIDEO_EXTENSIONS = {".webm",".mp4",".mov",".m4v",".mkv",".mpeg",".mpg"}
+
+class MediaProcessingError(Exception):
+    '''Raised when an uploaded video cannot be processed.'''
+
