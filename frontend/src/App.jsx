4888ef1bedaaad0219fd2dee47a7a72f1eb26e84
rudrakshia30 <rudrakshia30@gmail.com>
2026-07-08 21:55:44 +0530

    useref and useeffect

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState,useRef,useState } from "react";
 import RoleSelector from "./components/RoleSelector";
 import InterviewQuestions from "./components/InterviewQuestions";
 import { generateQuestions } from "./services/api";
