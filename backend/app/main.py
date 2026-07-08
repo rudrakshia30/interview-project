2276c4e420a6303094a4ea03c90f974c0d207556
rudrakshia30 <rudrakshia30@gmail.com>
2026-07-08 06:50:25 +0530

    importing video

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.routers.questions import router as questions_router
+from app.routers import video
 
 load_dotenv()
 
